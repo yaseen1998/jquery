e8d46fa5e8deebc6c1a38a7edcff56e96a2baeb0
yaseen1998 <Yasseenkouthe@ENG.HU.EDU.JO>
2022-03-14 12:58:26 +0200

    push

--- pages/jquery.js
+++ pages/jquery.js
@@ -15,22 +15,30 @@ export default function Jquery() {
         // $('#test').animate({
         //     top:'100px'}
         //     ,2000)
-        // $('button').click(()=>{
-        //     $('#test').stop(false,true)
+        // $('#button').click(()=>{
+        //     $('#test').fadeIn(2000)
         // })
-        var div = $('#test')
-        div
-        .slideUp(2000)
-        .slideDown(2000)
-        .fadeOut(2000)
-        .fadeIn(2000)
+        // var div = $('#test')
+        // div
+        // .slideUp(2000)
+        // .slideDown(2000)
+        // .fadeOut(2000)
+        // .fadeIn(2000)
+        // var p = $('p').text()
+        // $('span').text(p+'jquery')
+        $("a").attr('class','bg-blue-500')
+
+
     },[])
   return (
       <section>
     <div className="bg-red-500 relative" id="test">jquery</div>
+    input:<input type="text" id="input" />
+    <button id='button'>click</button>
     <p>animate</p>
-    <span className="text-cyan-700">effect</span>
-    <button>stop</button>
+    <a className='bg-red-500'>a</a>
+    {/* <span className="text-cyan-700">effect</span> */}
+    {/* <button>stop</button> */}
     </section>
   )
 }
